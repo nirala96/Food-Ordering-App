09aaa9f7988f6a00823fe775dbbdec62b6d24937
Arunoday kumar <201951031@iiitvadodara.ac.in>
2021-04-15 18:51:41 +0530

    added web

--- server/adddata.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'conn.php';
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-$conn->query("insert into login(username,password) values('".$username."','".$password."')");
-
-
-?>
