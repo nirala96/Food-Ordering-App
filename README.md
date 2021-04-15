5d4a4c608257e542373f559753b7599e8bc8be3f
Arunoday kumar <201951031@iiitvadodara.ac.in>
2021-04-15 12:37:22 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flutter UI | Beautiful Login and Signup Page UI/UX Design & Animation
+
---
+++ lib/Dashboard.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/cupertino.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
